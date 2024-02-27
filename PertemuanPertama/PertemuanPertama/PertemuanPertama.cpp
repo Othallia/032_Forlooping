2783af10076fc20a5696f43613a7ce63f27a8f2c
Othallia <vianiputriocta6@gmail.com>
2024-02-27 18:51:03 +0700

    Add project files.

---
+++ PertemuanPertama/PertemuanPertama/PertemuanPertama.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+	float nJejari, nLuas;
+		//numeric njejari, nluas
+	cout << "masukan nJejari = ";
+		//display "masukan jejari"
+	cin >> nJejari;
+		//accept "njejari"
+	nLuas = 3.14 * nJejari + nJejari;
+	cout << "luasnya =" << nLuas << endl;
+
+   
+}//end
+
